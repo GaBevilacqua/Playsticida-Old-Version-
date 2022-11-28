2b20212b78970be258b9c8dbd36a34bb682ada6b
Gabriel Bevilacqua Barros <gabriel.bevilacqua.2002@gmail.com>
2022-11-28 18:33:59 -0300

    Adição da tela de carregamento

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$ (window).on('load',function() {
-	document.getElementById("carregando").style.display = "none";
-	document.getElementById("corpo").style.display = "block";
-});
+var i = setInterval(function () {
+    
+    clearInterval(i);
+  
+    // O código desejado é apenas isto:
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("conteudo").style.display = "inline";
+
+}, 2500);
