eaecec75b6e04715a356fd7ff8dc7d042ba5f6e1
Gabriel Bevilacqua Barros <92558878+GabrielBBarros@users.noreply.github.com>
2023-05-02 10:50:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ var i = setInterval(function () {
     document.getElementById("conteudo").style.display = "inline";
 
 }, 2200);
+
+
+$(window).scroll(function() {
+    var posicao = $('.efeito').offset().top;
+    var alturaTela = $(window).height();
+    var alturaObjeto = $('.efeito').outerHeight();
+  
+    if ($(window).scrollTop() > posicao - alturaTela + alturaObjeto/2) {
+      $('.efeito').addClass('mostrar');
+    }
+  });
+
+  $(window).scroll(function() {
+    var posicao = $('.efeito-saida').offset().top;
+    var alturaTela = $(window).height();
+    var alturaObjeto = $('.efeito-saida').outerHeight();
+
+    if ($(window).scrollTop() > posicao - alturaTela + alturaObjeto/2) {
+        $('.efeito-saida').addClass('mostrar');
+    } else {
+        $('.efeito-saida').removeClass('mostrar');
+    }
+});
